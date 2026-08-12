e62da0275cf02d81c89bb8b7081c12b8755f9e62
Tamas Kery <tamas.kery@gmail.com>
2026-08-12 17:19:01 +0200

    Correct v0.6.0 release version date

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_coursecoach';
-$plugin->version = 2026081300;
+$plugin->version = 2026081203;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502];
 $plugin->maturity = MATURITY_BETA;
